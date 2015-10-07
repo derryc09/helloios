b95db4b62094a4849de7596ec392b73deaa42513
Andrew Yu <andrewyu@D-173-250-162-238.dhcp4.washington.edu>
2015-10-06 17:32:03 -0700

    adding app

---
+++ hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by Derry Cheng on 10/5/15.
+//  Copyright © 2015 Derry Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
